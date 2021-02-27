8a33fc25c46ad9d76837030796798ca99ae388d6
murilo0221 <muriloborges0202@gmail.com>
2021-02-27 00:41:14 -0300

    :package: tentando corrigr erro php

---
+++ email.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST{'email'}) && !empty($_POST['email'])){
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST["message"]);
+
+
+
+$to = "muriloborges0202@gmail.com";
+$subject = "Contato Webs Devoloper";
+$body = "Nome: " .$nome. "\n";
+        "Email: " .$email. "\n";
+        "mensagem: ".$mensagem;
+
+$header = "from:muriloborges088@gmail.com"."\r\n"."reply-to:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+
+
+if(mail($to,$subject,$body,$header)){
+    echo("E-mail enviado com sucesso!");
+
+}
+else{
+    echo("Desculpe, o E-mail não pode ser enviado");
+
+}
+
+}
+?>
