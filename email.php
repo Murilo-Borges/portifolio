988f6de14477e5d777371d4d3c921193ffd45cf2
murilo0221 <muriloborges0202@gmail.com>
2021-03-10 15:58:36 -0300

    alterações basicas

--- email.php
+++ email.php
@@ -10,18 +10,19 @@ $mensagem = addslashes($_POST["message"]);
 
 $to = "muriloborges0202@gmail.com";
 $subject = "Contato Webs Devoloper";
-$body = "Nome: " .$nome. "\n";
-        "Email: " .$email. "\n";
+$body = "Nome: " .$nome. "\n".
+        "Email: " .$email. "\n".
         "mensagem: ".$mensagem;
 
-$header = "from:muriloborges088@gmail.com"."\r\n"."reply-to:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+$header = "from:muriloborges088@gmail.com"."\r\n".
+        "reply-to:".$email."\r\n".
+        "X=Mailer:PHP/".phpversion();
 
 
 if(mail($to,$subject,$body,$header)){
     echo("E-mail enviado com sucesso!");
 
-}
-else{
+}else{
     echo("Desculpe, o E-mail não pode ser enviado");
 
 }
